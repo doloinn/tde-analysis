78bd1b72635b2fff14f1214fb6d845ffa0d73a5d
David Lynn <doloinn@gmail.com>
2017-02-28 18:08:01 +0000

    Light curve codes

--- output/unsampled_lightcurves.py
+++ output/unsampled_lightcurves.py
@@ -1,49 +1,12 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-import pickle
 
-def add_mags(m1, m2):
-    return -2.5 * np.log10(10**(-m1/2.5) + 10**(-m2/2.5))
+# Get tde dataframe
+from getdfs import get_tde_df
+tdedf = get_tde_df()
 
-tde_data = pickle.load(open('tdes.pickle', 'rb'))
-tde_sim = pd.read_csv('tde_parsed.csv', index_col=0)
-
-tde_sim.replace(-1.0000000150474662e+30, np.nan, inplace=True)
-
-tde_gal_data = [{}, {}]
-for key, val in tde_data.items():
-    # if key in tde_sim.index.values:
-    tde_gal_data[key % 2][key - (key % 2)] = val
-
-res_drop = ['multiple_matches', 'pix_sub-pix_pos', 'offset', 'ac_offset', 'al_offset', 'theta', 'ra', 'dec']
-gal_drop = ['ra', 'dec']
-
-simulated_data = {}
-for i, j in tde_sim.iloc[::2].iterrows():
-    temp_gal_dic = tde_gal_data[0][i]['data']
-    for key in gal_drop:
-        temp_gal_dic.pop(key)
-    # temp_gal_dic.pop('dec')
-    temp_gal_dic_in = {'in %s' % key: temp_gal_dic[key] for key in temp_gal_dic.keys()}
-
-    temp_tde_dic = tde_gal_data[1][i]['data']
-    temp_tde_dic_in = {'in %s' % key: temp_tde_dic[key] for key in temp_tde_dic.keys()}
-
-    temp_res_dic = j.to_dict()
-    for key in res_drop:
-        temp_res_dic.pop(key)
-    # temp_res_dic.pop('pix_sub-pix_pos')
-    temp_res_dic_out = {'out %s' % key: temp_res_dic[key] for key in temp_res_dic.keys()}
-
-    simulated_data[i] = {**temp_gal_dic_in, **temp_tde_dic_in, **temp_res_dic_out, 'tde source_g_mag': tde_sim.get_value(i + 1, 'source_g_mag')}
-    # print(simulated_data)
-
-tdedf = pd.DataFrame.from_dict(simulated_data, orient='index')
-
-tdedf.rename(columns={'out source_g_mag': 'galaxy source_g_mag'}, inplace=True)
-tdedf['total source_g_mag'] = tdedf.apply(lambda row: add_mags(row['tde source_g_mag'], row['galaxy source_g_mag']), axis=1)
-
+# Drop NaNs for detected sources
 tdedf_detected = tdedf.dropna()
 
 # Whole light curve plots
